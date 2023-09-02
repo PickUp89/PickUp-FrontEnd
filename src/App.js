35d5354bdf7c19fc8db8718900fd5220a47e645b
jayphan14 <kha21052004@gmail.com>
2023-09-02 16:29:15 -0400

    set up basic routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
-function App() {
-  return <h1> Hello World</h1>;
-}
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Authentication from "./pages/Authentication/Authentication";
+import Home from "./pages/Home/Home";
+import { connect } from "react-redux";
 
-export default App;
+const App = ({ isAuthenticated }) => {
+  return (
+    <Router>
+      <Routes>
+        {isAuthenticated ? (
+          <>
+            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Navigate to="/home" />} />
+          </>
+        ) : (
+          <>
+            <Route path="/auth" element={<Authentication />} />
+            <Route path="/" element={<Navigate to="/auth" />} />
+          </>
+        )}
+      </Routes>
+    </Router>
+  );
+};
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.user.isAuthenticated,
+});
+
+export default connect(mapStateToProps)(App);
