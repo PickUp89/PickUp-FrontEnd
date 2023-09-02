7c823198338e28503221c210db047a85c0994bfc
jayphan14 <kha21052004@gmail.com>
2023-09-02 15:26:51 -0400

    boilerplate for redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import ReactDOM from "react-dom";
+// import { Provider } from "react-redux";
+// import store from "./store";
 import App from "./App";
 
+// <Provider store={store}>
+//   <App />
+// </Provider>,
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
---
+++ src/store/store.js
@@ -0,0 +1,15 @@
+// src/store.js
+
+// import { configureStore } from "@reduxjs/toolkit";
+
+// // Import your reducers here
+// // import someReducer from './reducers/someReducer';
+
+// const store = configureStore({
+//   reducer: {
+//     // Add your reducers here
+//     // some: someReducer,
+//   },
+// });
+
+// export default store;
