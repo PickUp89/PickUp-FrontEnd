f3050208d3e0113ef4eac089cf06f4c1f07be878
jayphan14 <kha21052004@gmail.com>
2023-09-03 10:04:17 -0400

    config redux persist

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import store from "../src/store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "../src/store/store";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,41 @@
 import { configureStore } from "@reduxjs/toolkit";
-
+import { combineReducers } from "redux";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer from "../reducers/authSlice";
 
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const combinedReducer = combineReducers({
+  user: userReducer,
+  // Add other reducers here
 });
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, combinedReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+let persistor = persistStore(store);
+
+export { store, persistor };
