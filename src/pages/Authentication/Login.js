c5de9ac1aa2d645fb847d1ef993b290ced53e57c
jayphan14 <kha21052004@gmail.com>
2023-09-03 09:46:57 -0400

    login

--- src/pages/Authentication/Login.js
+++ src/pages/Authentication/Login.js
@@ -1,34 +1,45 @@
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
+
+import { loginWithEmailAsync } from "../../thunks/loginWithEmailAsyncThunk";
+import { useDispatch, useSelector } from "react-redux";
+
+import {
+  Avatar,
+  Button,
+  Alert,
+  CssBaseline,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  Grid,
+  Box,
+  Typography,
+  Container,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const defaultTheme = createTheme();
 
 export default function SignIn({ toggleAuthMode }) {
+  const dispatch = useDispatch();
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
+    const userEmail = data.get("email");
+    const userPassword = data.get("password");
+    dispatch(loginWithEmailAsync({ email: userEmail, password: userPassword }));
   };
+
+  const apiError = useSelector((state) => state.user.error);
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
+        {apiError && <Alert severity="error">{apiError}</Alert>}
         <Box
           sx={{
             marginTop: 8,
