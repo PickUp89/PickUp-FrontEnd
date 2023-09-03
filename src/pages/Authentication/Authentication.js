cdbddb90846b294c73e26c948d613e019c97ea5f
jayphan14 <kha21052004@gmail.com>
2023-09-03 12:21:33 -0400

    sign up

--- src/pages/Authentication/Authentication.js
+++ src/pages/Authentication/Authentication.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Login from "./Login";
-import Register from "./SignUp";
+import Register from "./Register";
 
 export const authModeEnum = {
   LOGIN: "LOGIN",
