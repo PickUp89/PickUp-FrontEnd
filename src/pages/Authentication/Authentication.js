a077ed5ac0c90fc3da1a0be6f6c33636a34af816
jayphan14 <kha21052004@gmail.com>
2023-09-02 23:11:51 -0400

    layout

--- src/pages/Authentication/Authentication.js
+++ src/pages/Authentication/Authentication.js
@@ -1,14 +1,25 @@
 import React, { useState } from "react";
 import Login from "./Login";
-import Register from "./Register";
+import Register from "./SignUp";
 
-const authModelEnum = {
+export const authModeEnum = {
   LOGIN: "LOGIN",
   REGISTER: "REGISTER",
 };
 const Authentication = () => {
-  const [authMode, setAuthMode] = useState(authModelEnum.LOGIN);
-  return authMode === authModelEnum.LOGIN ? <Login /> : <Register />;
+  const [authMode, setAuthMode] = useState(authModeEnum.LOGIN);
+  const toggleAuthMode = () => {
+    setAuthMode(
+      authMode === authModeEnum.LOGIN
+        ? authModeEnum.REGISTER
+        : authModeEnum.LOGIN
+    );
+  };
+  return authMode === authModeEnum.LOGIN ? (
+    <Login toggleAuthMode={toggleAuthMode} />
+  ) : (
+    <Register toggleAuthMode={toggleAuthMode} />
+  );
 };
 
 export default Authentication;
