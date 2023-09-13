affc58027c3cde547a7e740505688afa05e7c2cc
jayphan14 <kha21052004@gmail.com>
2023-09-13 08:09:04 -0400

    fix

--- src/Components/UpcomingGames/UpcomingGames.js
+++ src/Components/UpcomingGames/UpcomingGames.js
@@ -1,39 +1,8 @@
 import React from "react";
 import map from "lodash/map";
 import range from "lodash/range";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import { Paper } from "@mui/material";
 import EventCard from "../EventCard/EventCard";
-
-
-const cards = [
-    {
-      id: 1,
-      title: "",
-      description: "",
-      imageUrl: process.env.PUBLIC_URL + "/SportsPhotos/Photo2.png",
-      action: "Meet New People",
-    },
-    {
-      id: 2,
-      title: "",
-      description: "",
-      imageUrl: process.env.PUBLIC_URL + "/SportsPhotos/Photo3.jpeg",
-      action: "Stay Active",
-    },
-    {
-      id: 3,
-      title: "",
-      description: "",
-      imageUrl: process.env.PUBLIC_URL + "/SportsPhotos/Photo4.webp",
-      action: "Try a new sport",
-    },
-  ];
 
 export default function UpcomingGames() {
   return (
