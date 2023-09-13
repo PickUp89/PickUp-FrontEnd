b473e5c90178b7b04ec5cd7fb6b7e6b076746ca5
jayphan14 <kha21052004@gmail.com>
2023-09-13 10:03:00 -0400

    make it case sensitive

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,76 @@
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import { Facebook, Instagram, Twitter } from "@mui/icons-material";
+import { Box } from "@mui/material";
+
+export default function Footer() {
+  return (
+    <Box
+      component="footer"
+      sx={{
+        backgroundColor: (theme) =>
+          theme.palette.mode === "light"
+            ? theme.palette.grey[200]
+            : theme.palette.grey[800],
+        p: 6,
+      }}
+    >
+      <Container maxWidth="lg">
+        <Grid container spacing={5}>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              About Us
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Pickup is your sports community hub, connecting players for casual
+              games and sports-related activities. Join us to find and
+              participate in local pickup games and events today!
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              Contact Us
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              University of Waterloo, MC, University Avenue West, Waterloo, ON
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Email: PickUp@gmail.com
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Typography variant="h6" color="text.primary" gutterBottom>
+              Follow Us
+            </Typography>
+            <Link href="https://www.facebook.com/" color="inherit">
+              <Facebook />
+            </Link>
+            <Link
+              href="https://www.instagram.com/"
+              color="inherit"
+              sx={{ pl: 1, pr: 1 }}
+            >
+              <Instagram />
+            </Link>
+            <Link href="https://www.twitter.com/" color="inherit">
+              <Twitter />
+            </Link>
+          </Grid>
+        </Grid>
+        <Box mt={5}>
+          <Typography variant="body2" color="text.secondary" align="center">
+            {"Copyright © "}
+            <Link color="inherit" href="https://your-website.com/">
+              PickUp
+            </Link>{" "}
+            {new Date().getFullYear()}
+            {"."}
+          </Typography>
+        </Box>
+      </Container>
+    </Box>
+  );
+}
---
+++ src/components/UpcomingGames/UpcomingGames.js
@@ -0,0 +1,28 @@
+import React from "react";
+import map from "lodash/map";
+import range from "lodash/range";
+import Typography from "@mui/material/Typography";
+import EventCard from "../EventCard/EventCard";
+
+export default function UpcomingGames() {
+  return (
+    <>
+      <Typography variant="h2" align="center" marginBottom={5} marginTop={5}>
+        Upcoming PickUp Games
+      </Typography>
+      <div style={{ width: "100%", overflow: "auto", display: "flex" }}>
+        {map(range(6), (_) => (
+          <Container key={_} />
+        ))}
+      </div>
+    </>
+  );
+}
+
+const Container = () => {
+  return (
+    <div style={{ height: "30rem", width: "40rem", margin: "2rem" }}>
+      <EventCard />
+    </div>
+  );
+};
