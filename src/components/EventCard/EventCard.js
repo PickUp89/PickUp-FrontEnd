9ae6025bd02f931a2d9e308564647c59561d1903
jayphan14 <kha21052004@gmail.com>
2023-09-15 14:00:05 -0400

    handle click on event cards

--- src/components/EventCard/EventCard.js
+++ src/components/EventCard/EventCard.js
@@ -8,8 +8,12 @@ import Typography from "@mui/joy/Typography";
 import BookmarkAdd from "@mui/icons-material/BookmarkAddOutlined";
 import formatISODateToCustomString from "../../utils/formatISODateToCustomString";
 import testImage from "./testImage";
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from "react-redux";
 
-export default function EventCard({eventInfo, height, width}) {
+export default function EventCard({ eventInfo, height, width }) {
+  const isAuthenticated = useSelector((state) => state?.user?.isAuthenticated);
+  const navigate = useNavigate();
   const defaultEventInfo = {
     name: "Soccer Pick Up",
     date: "2023-08-31 17:35:10.895 -0400",
@@ -20,22 +24,50 @@ export default function EventCard({eventInfo, height, width}) {
     level: "Advanced",
   };
 
+  const renderLocation = (location) => {
+    const locationParts = location.split(","); // Split the location string at the first comma
+    const firstPart = locationParts[0].trim(); // Trim to remove any leading/trailing spaces
+    const secondPart = locationParts.slice(1).join(",").trim(); // Join the remaining parts and trim
+
+    // Then in your JSX code:
+    return (
+      <>
+        <Typography level="body-sm">{firstPart}</Typography>
+        <Typography level="body-sm">{secondPart}</Typography>
+      </>
+    );
+  };
+
+  const handleGoToPost = () => {
+    if (!isAuthenticated) navigate('/auth');;
+  };
+
+  const handleAttendEvent = () => {
+    if (!isAuthenticated) navigate('/auth');;
+  };
   const { name, date, attendees, location, coverImage, level } =
     eventInfo || defaultEventInfo;
   return (
-    <Card sx={{ width: width || 500, height: height || null,  backgroundColor: "white",}}>
+    <Card
+      sx={{
+        width: width || 500,
+        height: height || null,
+        backgroundColor: "white",
+      }}
+    >
       <div>
         <Typography level="title-lg">{name}</Typography>
         <Typography level="body-sm">
           {formatISODateToCustomString(date)}
         </Typography>
-        <Typography level="body-sm">{location}</Typography>
+        {renderLocation(location)}
         <IconButton
           aria-label="bookmark Bahamas Islands"
           variant="plain"
           color="neutral"
           size="sm"
           sx={{ position: "absolute", top: "0.875rem", right: "0.5rem" }}
+          onClick={handleAttendEvent}
         >
           <BookmarkAdd />
         </IconButton>
@@ -49,7 +81,7 @@ export default function EventCard({eventInfo, height, width}) {
             {level}
           </Typography>
           <Typography fontSize="sm" level="body-xs">
-            {attendees.length || 0} Attendees
+            {eventInfo?.attendeesNumber || attendees.length || 0} Attendees
           </Typography>
         </div>
         <Button
@@ -57,7 +89,13 @@ export default function EventCard({eventInfo, height, width}) {
           size="sm"
           color="primary"
           aria-label="Explore Bahamas Islands"
-          sx={{ ml: "auto", alignSelf: "center", fontWeight: 600, marginTop: 'auto', }}
+          onClick={handleGoToPost}
+          sx={{
+            ml: "auto",
+            alignSelf: "center",
+            fontWeight: 600,
+            marginTop: "auto",
+          }}
         >
           Explore
         </Button>
